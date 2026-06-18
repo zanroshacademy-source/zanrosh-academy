de746229478ecb03e35c4280869d3398d694d856
zanroshacademy-source <zanroshacademy@gmail.com>
2026-06-18 11:59:13 -0700

    done

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+      "react/display-name": "off"
+    }
+  },
+  {
     ignores: [
       ".next/**",
       "out/**",
